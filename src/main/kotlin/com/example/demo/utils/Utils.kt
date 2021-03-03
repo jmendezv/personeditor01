367dfd1fe664621e3c24a276104efb6a090def42
Pep Mendez <jmendez1@xtec.cat>
2021-03-03 18:40:17 +0100

    Implemented resources and dialogs

--- src/main/kotlin/com/example/demo/utils/Utils.kt
+++ src/main/kotlin/com/example/demo/utils/Utils.kt
@@ -4,92 +4,30 @@ import com.example.demo.database.PersonEntity
 import com.example.demo.model.Person
 import javafx.geometry.Pos
 import javafx.scene.Node
+import javafx.scene.control.Button
 import javafx.util.Duration
 import org.controlsfx.control.Notifications
 import org.controlsfx.control.action.Action
+import org.eclipse.fx.core.Tuple
+
+import javafx.scene.control.TextInputDialog
+import java.util.*
+
 
 object Utils {
 
     internal fun Person.toEntity(): PersonEntity = PersonEntity(id, name, title)
 
-    internal fun notification(
-        title: String?,
-        text: String?,
-        graphic: Node?,
-        position: Pos = Pos.BOTTOM_RIGHT,
-        hideAfter: Duration = Duration.seconds(5.0),
-        darkStyle: Boolean = false, owner: Any?, vararg actions: Action
-    ): Notifications {
-        val notification = Notifications
-            .create()
-            .title(title ?: "")
-            .text(text ?: "")
-            .graphic(graphic)
-            .position(position)
-            .hideAfter(hideAfter)
-            .action(*actions)
-        if (owner != null)
-            notification.owner(owner)
-        if (darkStyle)
-            notification.darkStyle()
-        return notification
-    }
-
-    fun warningNotification(
-        title: String?,
-        text: String?,
-        position: Pos = Pos.BOTTOM_RIGHT,
-        hideAfter: Duration = Duration.seconds(5.0),
-        darkStyle: Boolean = false, owner: Any? = null, vararg actions: Action
-    ) {
-        notification(title, text, null, position, hideAfter, darkStyle, owner, *actions)
-            .showWarning()
-    }
-
-    fun infoNotification(
-        title: String?,
-        text: String?,
-        position: Pos = Pos.BOTTOM_RIGHT,
-        hideAfter: Duration = Duration.seconds(5.0),
-        darkStyle: Boolean = false, owner: Any? = null, vararg actions: Action
-    ) {
-        notification(title, text, null, position, hideAfter, darkStyle, owner, *actions)
-            .showInformation()
-    }
-
-    fun confirmNotification(
-        title: String?,
-        text: String?,
-        position: Pos = Pos.BOTTOM_RIGHT,
-        hideAfter: Duration = Duration.seconds(5.0),
-        darkStyle: Boolean = false, owner: Any? = null, vararg actions: Action
-    ) {
-        notification(title, text, null, position, hideAfter, darkStyle, owner, *actions)
-            .showConfirm()
-    }
-
-    fun errorNotification(
-        title: String?,
-        text: String?,
-        position: Pos = Pos.BOTTOM_RIGHT,
-        hideAfter: Duration = Duration.seconds(5.0),
-        darkStyle: Boolean = false, owner: Any? = null, vararg actions: Action
-    ) {
-        notification(title, text, null, position, hideAfter, darkStyle, owner, *actions)
-            .showError()
-    }
-
-    fun customNotification(
-        title: String?,
-        text: String?,
-        graphic: Node,
-        position: Pos = Pos.BOTTOM_RIGHT,
-        hideAfter: Duration = Duration.seconds(5.0),
-        darkStyle: Boolean = false, owner: Any? = null,
-        vararg actions: Action
-    ) {
-        notification(title, text, graphic, position, hideAfter, darkStyle, owner, *actions)
-            .show()
+    fun getName(title: String, header: String): String? {
+        var returnedName: String? = null
+        val td = TextInputDialog("Nothing")
+        td.title = title
+        td.headerText = header
+        val result: Optional<String> = td.showAndWait()
+        if (result.isPresent) {
+            returnedName = td.editor.text
+        }
+        return returnedName
     }
 
 }
--- src/main/kotlin/com/example/demo/view/PersonEditor.kt
+++ src/main/kotlin/com/example/demo/view/PersonEditor.kt
@@ -1,51 +1,59 @@
 package com.example.demo.view
 
 import com.example.demo.controller.PersonController
-import com.example.demo.database.PersonDao
 import com.example.demo.model.Person
 import com.example.demo.model.PersonModel
+import javafx.application.Platform
 import javafx.collections.ObservableList
-import javafx.scene.control.TableView
+import javafx.geometry.Pos
 import javafx.scene.control.TreeItem
+import javafx.scene.input.KeyCombination
 import javafx.scene.layout.BorderPane
-import org.controlsfx.control.Notifications
 import tornadofx.*
-import tornadofx.controlsfx.breadcrumbbar
 import tornadofx.controlsfx.infoNotification
-import tornadofx.controlsfx.treeitem
-import javax.management.Notification
 
 class PersonEditor : View("Person Editor") {
     override val root: BorderPane = BorderPane()
     private val persons: ObservableList<Person>
     private val model: PersonModel = PersonModel(Person())
     private val controller: PersonController by inject()
-//    var targetCrumb: TreeItem<String>? = null
+    var targetCrumb: TreeItem<String>? = null
 
     init {
         persons = controller.persons()
         with(root) {
             top {
-//                breadcrumbbar<String> {
-//                    treeitem("Alpha") {
-//                        treeitem("Beta") {
-//                            targetCrumb = treeitem("Gamma")
-//                        }
-//                        treeitem("Zeta")
-//                    }
-//                    selectedCrumb = targetCrumb
-//                }
+                menubar {
+                    menu("File") {
+                        menu("Connect") {
+                            item("Facebook")
+                            item("Twitter")
+                        }
+                        separator()
+                        item("Save", KeyCombination.valueOf("Shortcut+S"))
+                        item("Quit", KeyCombination.valueOf("Shortcut+Q")).action {
+                            Platform.exit()
+                        }
+                    }
+                    menu("Edit") {
+                        item("Copy")
+                        item("Paste")
+                    }
+                }
             }
             center {
                 tableview(persons) {
                     column("Name", Person::nameProperty)
                     column("Title", Person::titleProperty)
-                    // Actualiza la Person de dentro del modelo
-//                    model.rebindOnChange(this) { personaSeleccionada ->
-//                        item = personaSeleccionada ?: Person()
-//                    }
-                    // Cuando el modelo extiende ItemViewModel
                     bindSelected(model)
+                    contextmenu {
+                        item("Send Email").action {
+                            selectedItem?.apply { println("Sending Email to $name") }
+                        }
+                        item("Change Status").action {
+                            selectedItem?.apply { println("Changing Status for $name") }
+                        }
+                    }
                 }
             }
 
@@ -66,13 +74,13 @@ class PersonEditor : View("Person Editor") {
                                 model.item = Person()
                             }
                             button("Add") {
-                                // model.dirty vol dir que ha canviat name/title
-                                enableWhen(model.dirty)
+                                enableWhen(model.valid)
                                 action {
                                     save()
                                 }
                             }
                             button("Delete") {
+                                enableWhen(model.valid)
                                 action {
                                     delete()
                                 }
@@ -95,9 +103,7 @@ class PersonEditor : View("Person Editor") {
 
     override fun onDock() {
         super.onDock()
-        currentWindow?.setOnCloseRequest {
-            controller.closeConnection()
-        }
+        currentWindow?.setOnCloseRequest { controller.closeConnection() }
     }
 
     private fun update() {
@@ -111,16 +117,41 @@ class PersonEditor : View("Person Editor") {
             persons.sortBy {
                 it.name
             }
+            infoNotification(messages["person_editor"], messages["el_registro_se_ha_actualizado_correctamente"], Pos.CENTER)
+        } else {
+            infoNotification(messages["person_editor"], messages["no_hay_nada_que_actualizar"], Pos.CENTER)
         }
     }
 
     private fun save() {
-        model.commit()
-        val person = model.item
-        controller.save(person)
-        persons.add(person)
-        persons.sortBy {
-            it.name
+        val oldPerson = model.item
+        if (oldPerson.id == 0) {
+            model.commit()
+            if (!model.isValid) {
+                infoNotification(
+                    messages["person_editor"],
+                    messages["los_campos_no_pueden_quedar_en_blanco"],
+                    Pos.CENTER
+                )
+                return
+            }
+            val person = model.item
+            controller.save(person)
+            persons.add(person)
+            persons.sortBy {
+                it.name
+            }
+            infoNotification(
+                messages["person_editor"],
+                messages["el_registro_se_ha_guardado_satisfactoriamente"],
+                Pos.CENTER
+            )
+        } else {
+            infoNotification(
+                messages["person_editor"],
+                messages["no_se_puede_duplicar_el_registro"],
+                Pos.CENTER
+            )
         }
     }
 
@@ -128,8 +159,17 @@ class PersonEditor : View("Person Editor") {
         model.commit()
         val person = model.item
         if (person.id > 0) {
-            controller.delete(person)
-            persons.remove(person)
+            messages
+            confirm(title = messages["person_editor"], header = messages["estas_seguro"]) {
+                controller.delete(person)
+                persons.remove(person)
+                information(title = messages["person_editor"],
+                    header = messages["el_registro_se_ha_eliminado_correctamente"],
+                    content = """${person.name} ${messages["se_ha_marcado_como_borrado"]}"""
+                )
+            }
+        } else {
+            infoNotification(messages["person_editor"], messages["el_registro_no_esta_presente_en_la_tabla"], Pos.CENTER)
         }
     }
 
